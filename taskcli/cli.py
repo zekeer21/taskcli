a1959586555c8cc6f6dca328e4b5e729f6d82225
zekeer21 <achas241@gmail.com>
2025-05-01 12:06:01 +0800

    Added function to handle delete and clearing tasks

--- taskcli/cli.py
+++ taskcli/cli.py
@@ -1,5 +1,5 @@
 import argparse
-from taskcli.core import add, update, mark, list_tasks
+from taskcli.core import add, update, mark, list_tasks, delete, clear
 
 
 def handle_add(args):
@@ -22,6 +22,14 @@ def handle_list(args):
     list_tasks(args.status)
 
 
+def handle_delete(args):
+    delete(args.id)
+
+
+def handle_clear(args):
+    clear()
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Task CLI: add, update, mark, and list your tasks."
@@ -38,6 +46,18 @@ def main():
     update_parser.add_argument("id", type=int, help="ID of the task to update")
     update_parser.add_argument("description", type=str, help="New description")
     update_parser.set_defaults(func=handle_update)
+
+    # Delete command
+    delete_parser = subparsers.add_parser("delete", help="Delete a task")
+    delete_parser.add_argument("id", type=int, help="ID of the task to delete")
+    delete_parser.set_defaults(func=handle_delete)
+
+    # Clear command
+    clear_parser = subparsers.add_parser("clear", help="Clear all tasks")
+    clear_parser.set_defaults(func=handle_clear)
+    clear_parser.add_argument(
+        "-y", "--yes", action="store_true", help="Confirm clearing all tasks"
+    )
 
     # Mark in-progress command
     mark_in_progress_parser = subparsers.add_parser(
