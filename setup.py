0e7afa05e3142da6de91aac5e94c6630193bb1e0
zekeer21 <achas241@gmail.com>
2025-05-01 12:05:22 +0800

    Added minor modifications

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-# setup.py
-
 from setuptools import setup, find_packages
 
 setup(
     name="taskcli",
-    version="0.1",
+    version="0.1.0",
+    description="Simple task manager CLI tool",
+    author="Your Name",
     packages=find_packages(),
-    install_requires=[],  # if you use external packages later
+    install_requires=[],
     entry_points={
         "console_scripts": [
             "taskcli=taskcli.cli:main",
         ],
     },
 )
+# This setup script uses setuptools to package the taskcli application.
